f3c0e85c26ca47ded7d328a789be91a69f1de085
aniketgupta-25 <aniketgupta51892@gmail.com>
2024-12-25 02:26:37 +0530

    Delete test-main/Axion directory

--- test-main/Axion/middleware/authorization.mw.js
+++
@@ -1,9 +0,0 @@
-// authorization.middleware.js
-const roleAuth = (allowedRoles) => {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return ResponseHandler.error(res, 'Unauthorized access', 403);
-    }
-    next();
-  };
-};
